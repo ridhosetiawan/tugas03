f061e8d397684be979df3b0e5727c4b796a41aff
ahmadridhosetiawan <32403430+ahmadridhosetiawan@users.noreply.github.com>
2017-11-05 19:42:27 +0800

    Menghitung serta menampilkan selisih x-y atau y-x

---
+++ Untitled1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int x,y,z;
+	cout << "nilai x = "; cin >> x;
+	cout << "nilai y = "; cin >> y;
+	
+	if (x>y){
+		z=x-y;
+		cout << "x-y = " << z;
+	}else{
+		z=y-x;
+		cout << "y-x = " << z;
+	}
+}
